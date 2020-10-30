c0e1b41a228203a159321f93fed96fa04651e871
TJ <tmoyers1@gmail.com>
2020-10-29 23:41:45 -0400

    Added dynamic data from db to the Products page table

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customers(request):
     return render(request, 'accounts/customers.html')
