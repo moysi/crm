394521657698b2d018fcd1d11877ad8cbda98ed4
TJ <tmoyers1@gmail.com>
2020-10-29 22:35:48 -0400

    First commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+appname = "accounts"
+# Create your views here.
+urlpatterns = [
+    path('', views.home),
+    path('products/', views.products, name='products'),
+    path('customers/', views.customers),
+]
